f7948cc5f1e7ae865f27c31fa465678f68de1bdb
junnopham <phamdinhson07@gmail.com>
2022-05-25 14:12:50 +0700

    Update base controller

--- server/src/controllers/base.controller.js
+++ server/src/controllers/base.controller.js
@@ -17,11 +17,11 @@ module.exports = class BaseController {
         status: "success",
         data,
       });
-    } catch (error) {
-      return res.status(400).json({
-        code: 400,
+    } catch (e) {
+      return res.status(500).json({
+        code: 500,
         status: "error",
-        message: "Error when fetch data, please try again later!",
+        message: e.message || "Some error occurred while retrieving data",
       });
     }
   }
@@ -31,34 +31,109 @@ module.exports = class BaseController {
       const { id } = req.params;
       const data = await this.model.findById(id).lean();
 
-      if (!data) throw new Error("Not found");
+      if (!data) throw new Error("Data not found with id " + id);
 
       return res.json({
         code: 200,
         status: "success",
         data,
       });
-    } catch (error) {
-      return res.status(400).json({
-        code: 400,
+    } catch (e) {
+      if (e.kind === "ObjectId") {
+        return res.status(404).send({
+          code: 404,
+          status: "error",
+          message: "Data not found with id " + req.params.id,
+        });
+      }
+      return res.status(500).send({
+        code: 500,
         status: "error",
-        message: "Error when fetch data, please try again later!",
+        message: "Error retrieving data with id " + req.params.id,
       });
     }
   }
 
   async create(req, res) {
     try {
-    } catch {
-      return res.status(400).json({
-        code: 400,
+      const data = req.body;
+
+      const newData = new this.model(data);
+      await newData.save();
+
+      return res.json({
+        code: 200,
+        status: "success",
+        message: "Create data successfully!",
+        data: newData,
+      });
+    } catch (e) {
+      res.status(500).send({
+        code: 500,
         status: "error",
-        message: "Error when insert data, please try again later!",
+        message: e.message || "Some error occurred while creating data.",
       });
     }
   }
 
-  async update(req, res) {}
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const { body } = req.body;
+      const data = await this.model.findByIdAndUpdate(id, body, {
+        new: true,
+      });
 
-  async delete(req, res) {}
+      if (!data) throw new Error("Data not found with id " + id);
+
+      return res.json({
+        code: 200,
+        status: "success",
+        message: "Data updated successfully!",
+        data,
+      });
+    } catch (e) {
+      if (e.kind === "ObjectId") {
+        return res.status(404).send({
+          code: 404,
+          status: "error",
+          message: "Note not found with id " + req.params.id,
+        });
+      }
+      return res.status(500).send({
+        code: 500,
+        status: "error",
+        message: "Error updating note with id " + req.params.id,
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      const data = await this.model.findByIdAndRemove(id);
+
+      if (!data) throw new Error("Data not found with id " + id);
+
+      return res.send({
+        code: 200,
+        status: "success",
+        message: "Data deleted successfully!",
+      });
+    } catch (e) {
+      if (e.kind === "ObjectId" || e.name === "NotFound") {
+        return res.status(404).send({
+          code: 404,
+          status: "error",
+          message: "Data not found with id " + req.params.id,
+        });
+      }
+      return res.status(500).send({
+        code: 500,
+        status: "error",
+        message: "Could not delete data with id " + req.params.id,
+      });
+    }
+  }
 };
